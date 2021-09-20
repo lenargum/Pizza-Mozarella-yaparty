9a7e07f027c213f7da12d8670e1e182a0ffca09b
lenar <lenargamgam@gmail.com>
2021-09-20 16:01:07 +0300

    added color and disabled option

--- frontend/src/components/BigFab.vue
+++ frontend/src/components/BigFab.vue
@@ -6,6 +6,8 @@
     :height="400"
     :width="400"
     @click="$emit('click')"
+    :disabled="!active"
+    :color="color"
   >
     <h2 class="text-h4">{{ text }}</h2>
   </v-btn>
@@ -16,6 +18,14 @@ export default {
   name: "BigFab",
   props: {
     text: {
+      type: String,
+      default: ''
+    },
+    active: {
+      type: Boolean,
+      default: true
+    },
+    color: {
       type: String,
       default: ''
     }
